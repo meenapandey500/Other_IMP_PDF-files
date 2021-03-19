fbedbfdfee45097238716b6d1d07f180c596018d
meenapandey500 <62098816+meenapandey500@users.noreply.github.com>
2021-03-19 15:21:33 +0530

    Add files via upload

---
+++ dic.py
@@ -0,0 +1,9 @@
+#Define a function which can print a dictionary where the keys are numbers between 1 and 3
+#(both included) and the values are square of keys - done
+def func():
+    d={1:1*1,2:2*2,3:3*3}
+    print(d)
+
+#main program
+#call function
+func()
